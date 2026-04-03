5de16ba2a40f5df975f9411dd4ac0890519da87d
henry <henry.forman@tuwien.ac.at>
2026-04-03 10:20:09 +0200

    Add support for PPO algorithm and enhance model training parameters

--- afm_agent_training.py
+++ afm_agent_training.py
@@ -3,7 +3,7 @@ from env.afm_env import AfmEnvironment
 import argparse
 import tqdm
 from stable_baselines3.common.callbacks import CheckpointCallback, BaseCallback
-from stable_baselines3 import SAC
+from stable_baselines3 import SAC, PPO
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
 import os
@@ -15,22 +15,24 @@ parser.add_argument("--net_arch", type=int, nargs="+", default=[256, 256], help=
 parser.add_argument("--num_historic_data", type=int, default=30, help="Number of historic data points")
 parser.add_argument("--df_scale", type=float, default=10.0, help="Divisor for df observations")
 parser.add_argument("--dz_scale", type=float, default=10.0, help="Divisor for dz observations")
+parser.add_argument("--algorithm", type=str, default="sac", choices=["sac", "ppo"], help="RL algorithm to train")
+parser.add_argument("--epsilon", type=float, default=0.2, help="PPO clip epsilon")
 args = parser.parse_args()
 
 TRAIN_BASE_DIR = "train_results"
 train_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 arch_str = "x".join(str(s) for s in args.net_arch)
-model_name = f"sac_afm_model_arch{arch_str}_hist{args.num_historic_data}_df{args.df_scale}_dz{args.dz_scale}_{train_date}"
+epsilon_suffix = f"_eps{args.epsilon}" if args.algorithm == "ppo" else ""
+model_name = f"{args.algorithm}_afm_model_arch{arch_str}_hist{args.num_historic_data}_df{args.df_scale}_dz{args.dz_scale}{epsilon_suffix}_{train_date}"
 TRAIN_DIR = os.path.join(TRAIN_BASE_DIR, model_name)
 os.makedirs(TRAIN_DIR, exist_ok=True)
 
 ENVIRONMENT_DIRS = [
-    # "environments/pt_111_small_single_adatom",
-    # "environments/pt_111_small_1_3_vacancies",
-    # "environments/pt_111_small_rows_missing",
-    # "environments/pt_111_small_5row_missing",
-    "environments/pt_111_small_rows_missing_single",
+    "environments/pt_111_1a",
+    "environments/pt_111_2v",
+    "environments/pt_111_2v_1a",
+    "environments/pt_111_3v_1a",
 ]
 
 def make_env_gpu(rank=0):
@@ -61,6 +63,8 @@ def make_env_load(rank=0):
             dz_scale=args.dz_scale,
             base_reward=10,
             crash_reward=-100.0,
+            sigma=2,
+            height_offset_reward=0.4,
         )
         env = Monitor(env)  #, filename=os.path.join("./logs", f"env_{rank}"))
         return env
@@ -70,35 +74,49 @@ def make_env_load(rank=0):
 n_envs = 4
 env_arr = [make_env_load(i) for i in range(n_envs)]
 vec_env = DummyVecEnv(env_arr)
-# vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=True, clip_obs=10.)
+vec_env = VecNormalize(vec_env, norm_obs=False, norm_reward=True, clip_obs=10.)
 
 policy_kwargs = dict(
     net_arch=args.net_arch,
     activation_fn=ReLU,
 )
 
-model = SAC(
-    "MultiInputPolicy",
-    vec_env,
-    verbose=1,
-    tensorboard_log="./tensorboard_logs_new_env",
-    policy_kwargs=policy_kwargs,
-    gradient_steps=n_envs,
-)
+if args.algorithm == "sac":
+    model = SAC(
+        "MultiInputPolicy",
+        vec_env,
+        verbose=1,
+        tensorboard_log="./tensorboard_logs_final_envs",
+        policy_kwargs=policy_kwargs,
+        gradient_steps=n_envs,
+        device="cuda",
+    )
+else:
+    model = PPO(
+        "MultiInputPolicy",
+        vec_env,
+        verbose=1,
+        tensorboard_log="./tensorboard_logs_final_envs",
+        policy_kwargs=policy_kwargs,
+        clip_range=args.epsilon,
+        ent_coef=0.01,
+        device="cuda",
+    )
 
 checkpoint_callback = CheckpointCallback(
     save_freq=10000,
     save_path=os.path.join(TRAIN_DIR, "models"),
     name_prefix=model_name,
     save_replay_buffer=False,
+    save_vecnormalize=True,
 )
 
 model.learn(
-    total_timesteps=10000000,
+    total_timesteps=50000000,
     log_interval=1,
     progress_bar=False,
     tb_log_name=model_name,#"sac_afm_env" + train_date,
     callback=[checkpoint_callback],
 )
 model.save(os.path.join(TRAIN_DIR, "final_model"))
-# vec_env.save(os.path.join(TRAIN_DIR, "vec_normalize.pkl"))
+vec_env.save(os.path.join(TRAIN_DIR, "vec_normalize.pkl"))
